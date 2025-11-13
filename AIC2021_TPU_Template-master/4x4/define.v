be89ad58255b9c9a22e949f6b3bd388dc4a56b60
Seungmin Lim <smlim789@korea.ac.kr>
2025-11-13 17:00:42 +0900

    Update define.v

--- AIC2021_TPU_Template-master/4x4/define.v
+++ AIC2021_TPU_Template-master/4x4/define.v
@@ -23,8 +23,8 @@
 `define DEFINE_V
 
 `define DATA_SIZE 8
-`define WORD_SIZE 32
-`define GBUFF_ADDR_SIZE 256
+`define WORD_SIZE 4 //수정
+`define GBUFF_ADDR_SIZE 4 //수정
 //`define GBUFF_INDX_SIZE (GBUFF_ADDR_SIZE/WORD_SIZE)
 `define GBUFF_INDX_SIZE 8
 `define GBUFF_SIZE (WORD_SIZE*GBUFF_ADDR_SIZE)
