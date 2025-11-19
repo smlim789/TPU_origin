85abff3e5492dd625f202a93e31cf187eb633224
Seungmin Lim <smlim789@korea.ac.kr>
2025-11-19 11:43:10 +0900

    Update define.v

--- AIC2021_TPU_Template-master/5x5/define.v
+++ AIC2021_TPU_Template-master/5x5/define.v
@@ -1,13 +1,23 @@
+//============================================================================//
+// AIC2021 Project1 - TPU Design                                              //
+// file: define.v                                                             //
+// description: All Definitions                                               //
+//============================================================================//
+
 `ifndef MATRIX_DEFINE_V
 `define MATRIX_DEFINE_V
-    // No include needed here for this fix
+    // Dimensions handled manually in TB, but defined here for safety
+    `define MATRIX_A_ROW 5
+    `define MATRIX_A_COL 5
+    `define MATRIX_B_ROW 5
+    `define MATRIX_B_COL 5
 `endif
 
 `ifndef DEFINE_V
 `define DEFINE_V
 
 `define DATA_SIZE 8
-`define WORD_SIZE 40  // Set to 40 bits for 5x5 Matrix (5 bytes)
+`define WORD_SIZE 40  // 5 bytes for 5x5 matrix
 `define GBUFF_ADDR_SIZE 256
 `define GBUFF_INDX_SIZE 8
 `define GBUFF_SIZE (WORD_SIZE*GBUFF_ADDR_SIZE)
