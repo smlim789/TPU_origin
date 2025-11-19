1485c08853ff906a571250ec72fa4a8ee31fde59
Seungmin Lim <smlim789@korea.ac.kr>
2025-11-19 11:22:44 +0900

    Update define.v

--- AIC2021_TPU_Template-master/5x5/define.v
+++ AIC2021_TPU_Template-master/5x5/define.v
@@ -1,48 +1,23 @@
-//============================================================================//
-// AIC2021 Project1 - TPU Design                                              //
-// file: define.v                                                             //
-// description: All Definations                                               //
-// authors: kaikai (deekai9139@gmail.com)                                     //
-//          suhan  (jjs93126@gmail.com)                                       //
-//============================================================================//
-
-//----------------------------------------------------------------------------//
-// Matrix Parameters Definations                                              //
-//----------------------------------------------------------------------------//
 `ifndef MATRIX_DEFINE_V
 `define MATRIX_DEFINE_V
-
-`include "matrix_define.v"
+    // No include needed here for this fix
 `endif
-
-//----------------------------------------------------------------------------//
-// Common Definations                                                         //
-//----------------------------------------------------------------------------//
 
 `ifndef DEFINE_V
 `define DEFINE_V
 
 `define DATA_SIZE 8
-`define WORD_SIZE 32 // *** CHANGED: Was 32 (4x8), now 40 (5x8) ***
+`define WORD_SIZE 40  // Set to 40 bits for 5x5 Matrix (5 bytes)
 `define GBUFF_ADDR_SIZE 256
-//`define GBUFF_INDX_SIZE (GBUFF_ADDR_SIZE/WORD_SIZE)
 `define GBUFF_INDX_SIZE 8
 `define GBUFF_SIZE (WORD_SIZE*GBUFF_ADDR_SIZE)
 
-// Buffer Sizing for 5x5
-// Left Buf: Latency for skewing (approx 2*N)
+// Buffer Sizing
 `define LEFT_BUF_SIZE 10 
-// Down Buf: Latency for collection (approx 3*N - 1)
 `define DOWN_BUF_SIZE 14 
 
-//----------------------------------------------------------------------------//
-// Simulations Definations                                                    //
-//----------------------------------------------------------------------------//
+// Simulation
 `define CYCLE 10
 `define MAX   500000
-
-//----------------------------------------------------------------------------//
-// User Definations                                                           //
-//----------------------------------------------------------------------------//
 
 `endif
