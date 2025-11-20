26bdb693139d092007e17f78f01663f1f6eac76a
Seungmin Lim <smlim789@korea.ac.kr>
2025-11-20 11:04:14 +0900

    Update define.v

--- AIC2021_TPU_Template-master/4x4/define.v
+++ AIC2021_TPU_Template-master/4x4/define.v
@@ -1,33 +1,42 @@
 //============================================================================//
 // AIC2021 Project1 - TPU Design                                              //
 // file: define.v                                                             //
-// description: All Definitions                                               //
+// description: All Definations                                               //
+// authors: kaikai (deekai9139@gmail.com)                                     //
+//          suhan  (jjs93126@gmail.com)                                       //
 //============================================================================//
 
+//----------------------------------------------------------------------------//
+// Matrix Parameters Definations                                              //
+//----------------------------------------------------------------------------//
 `ifndef MATRIX_DEFINE_V
 `define MATRIX_DEFINE_V
-    // Dimensions handled manually in TB, but defined here for safety
-    `define MATRIX_A_ROW 5
-    `define MATRIX_A_COL 5
-    `define MATRIX_B_ROW 5
-    `define MATRIX_B_COL 5
+
+`include "matrix_define.v"
 `endif
+
+//----------------------------------------------------------------------------//
+// Common Definations                                                         //
+//----------------------------------------------------------------------------//
 
 `ifndef DEFINE_V
 `define DEFINE_V
 
 `define DATA_SIZE 8
-`define WORD_SIZE 40  // 5 bytes for 5x5 matrix
+`define WORD_SIZE 32
 `define GBUFF_ADDR_SIZE 256
+//`define GBUFF_INDX_SIZE (GBUFF_ADDR_SIZE/WORD_SIZE)
 `define GBUFF_INDX_SIZE 8
 `define GBUFF_SIZE (WORD_SIZE*GBUFF_ADDR_SIZE)
 
-// Buffer Sizing
-`define LEFT_BUF_SIZE 10 
-`define DOWN_BUF_SIZE 14 
-
-// Simulation
+//----------------------------------------------------------------------------//
+// Simulations Definations                                                    //
+//----------------------------------------------------------------------------//
 `define CYCLE 10
 `define MAX   500000
+
+//----------------------------------------------------------------------------//
+// User Definations                                                           //
+//----------------------------------------------------------------------------//
 
 `endif
